5f5706369cb99c2599636320aebd8fc068c33aee
Sham Kumar <sham.kumar@brainxtech.com>
2023-08-22 16:44:04 +0500

    latest changes

--- Sleep_App_Demo/Extentions/UICollectionView.swift
+++ Sleep_App_Demo/Extentions/UICollectionView.swift
@@ -1,9 +1,9 @@
-import Foundation
 import UIKit
 
 extension UICollectionView {
     
     // MARK: - Internal Methods
+    
     func dequeue<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
         return dequeueReusableCell(withReuseIdentifier: T.reuseIdentifierForCollection, for: indexPath) as! T
     }
--- Sleep_App_Demo/Extentions/UICollectionViewCell.swift
+++ Sleep_App_Demo/Extentions/UICollectionViewCell.swift
@@ -3,11 +3,13 @@ import UIKit
 extension UICollectionViewCell {
     
     // MARK: - Static Properties
+    
     static var reuseIdentifierForCollection: String {
         return String(describing: self)
     }
     
     // MARK: - Static Methods
+    
     static func nibForCollection() -> UINib {
         return UINib(nibName: self.reuseIdentifierForCollection, bundle: nil)
     }
